f4b544d74f372021e926459cdbdb2f0c95c83ef1
tky-wsd <59105100+tky-wsd@users.noreply.github.com>
2019-12-24 00:30:01 +0900

    Modify argument

--- v4/egs/utils/prepare.sh
+++ v4/egs/utils/prepare.sh
@@ -2,11 +2,11 @@
 
 . ./path.sh
 
-original_train_path="$1"
+original_csv_path="$1"
 train_csv_path="$2"
 valid_csv_path="$3"
 
 make_data.py \
---original_train_path ${original_train_path} \
+--original_csv_path ${original_csv_path} \
 --train_csv_path ${train_csv_path} \
 --valid_csv_path ${valid_csv_path}
