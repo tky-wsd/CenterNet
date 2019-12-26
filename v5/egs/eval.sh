76e5f1c78d60a0906b4d2d5f2d084777830e5cdb
tky-wsd <59105100+tky-wsd@users.noreply.github.com>
2019-12-26 14:06:33 +0900

    Change training condition

--- v5/egs/eval.sh
+++ v5/egs/eval.sh
@@ -12,24 +12,19 @@ test_image_dir="${DATA_DIR}/test_images"
 # Network configuration
 S=1
 C=[64,64,128,128,128,256]
-H=256
-heatmap=0.0
+H=128
+heatmap=1.0
 local_offset=0.0
 depth=1.0
+yaw=0.0
+pitch=0.0
+roll=0.0
 potential_map=0
 batch_size=4
 
-if [ $potential_map -ne 0 ]; then
-    height=2710
-    width=3384
-    potential_map_option="--height $height --width $width"
-else
-    potential_map_option=""
-fi
-
-model_dir="${out_dir}/UNet_S${S}_C${C}_H${H}_heatmap${heatmap}_local_offset${local_offset}_depth${depth}"
+model_dir="${out_dir}/UNet_S${S}_C${C}_H${H}_heatmap${heatmap}_local_offset${local_offset}_depth${depth}_potential_map${potential_map}"
 model_path="${model_dir}/epoch${end_epoch}.pth"
-out_image_dir="${model_dir}/images"
+out_image_dir="${model_dir}/epoch${end_epoch}"
 
 eval_unet.py \
 --test_csv_path ${test_csv_path} \
@@ -38,13 +33,15 @@ eval_unet.py \
 --f_y 2305.8757 \
 --c_x 1686.2379 \
 --c_y 1354.9849 \
-${potential_map_option} \
 --S $S \
 --C $C \
 --H $H \
 --batch_size ${batch_size} \
---heatmap $heatmap \
+--heatmap ${heatmap} \
 --local_offset ${local_offset} \
---depth $depth \
+--depth ${depth} \
+--yaw ${yaw} \
+--pitch ${pitch} \
+--roll ${roll} \
 --model_path "${model_path}" \
 --out_image_dir "${out_image_dir}"
